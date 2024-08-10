a2bbbd2989306e191b29289857422e1701a9e076
Kapil <kbbhatt61@gmail.com>
2024-08-10 15:40:07 +0530

    simple api

---
+++ src/main/java/com/example/task_manager/controller/TaskController.java
@@ -0,0 +1,28 @@
+package com.example.task_manager.controller;
+
+import com.example.task_manager.model.Task;
+import com.example.task_manager.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+@CrossOrigin(origins = "http://localhost:3000")
+public class TaskController {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    // More endpoints can be added here
+}
---
+++ src/main/java/com/example/task_manager/model/Task.java
@@ -0,0 +1,17 @@
+package com.example.task_manager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private boolean completed;
+
+    // Getters and setters
+}
